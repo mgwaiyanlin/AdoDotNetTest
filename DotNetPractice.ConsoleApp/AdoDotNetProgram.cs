1e950b538c693ca747bbe5a9ab820980dcaf4c85
mgwaiyanlin <waiyanlinn.netuser@gmail.com>
2024-08-28 14:50:02 +0630

    Add project files.

---
+++ DotNetPractice.ConsoleApp/AdoDotNetProgram.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPractice.ConsoleApp
+{
+    internal class AdoDotNetProgram
+    {
+        private static SqlConnectionStringBuilder stringBuilder;
+        private static SqlConnection connection;
+        public AdoDotNetProgram()
+        {
+
+            stringBuilder = new SqlConnectionStringBuilder();
+            stringBuilder.DataSource = "localhost"; // server name
+            stringBuilder.InitialCatalog = "DotNetPractice"; // database name
+            stringBuilder.UserID = "myserver";
+            stringBuilder.Password = "password";
+            connection = new SqlConnection(stringBuilder.ConnectionString);
+
+            // alternative way of sql connection
+            //SqlConnection connection = new SqlConnection("stringBuilder.DataSource = \"localhost\";stringBuilder.InitialCatalog = \"DotNetPractice\";stringBuilder.UserID = \"myserver\";stringBuilder.Password = \"password\"; ");
+
+            connection.Open();
+
+        }
+
+        public void ReadData()
+        {
+            string query = "select * from blogs";
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            Console.WriteLine("\n------------------------------------");
+            Console.WriteLine("#### Querying Blog Database ####\n");
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                Console.WriteLine("id: " + dr["id"]);
+                Console.WriteLine("title: " + dr["title"]);
+                Console.WriteLine("author: " + dr["author"]);
+                Console.WriteLine("content: " + dr["blog_content"]);
+                Console.WriteLine("------------------------------------");
+            }
+            connection.Close();
+
+            Console.WriteLine("Database connection closed!");
+        }
+
+        
+    }
+}
---
+++ DotNetPractice.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using DotNetPractice.ConsoleApp;
+using System.Data;
+using System.Data.SqlClient;
+
+
+AdoDotNetProgram adoDotNetProgram = new AdoDotNetProgram();
+
+Console.WriteLine("\t*** Welcome to Our Database CRUD with AdoDotNet ***\n");
+
+Console.WriteLine("Choose the following one of numbers to take action: ");
+Console.WriteLine("0. Close the program");
+Console.WriteLine("1. Read Data");
+Console.WriteLine("2. Write Data");
+Console.WriteLine("3. Update Data");
+
+Console.Write("\nEnter a number: ");
+string user_input = Console.ReadLine();
+
+Console.WriteLine(user_input);
